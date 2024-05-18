f7890f79567be4b2ffae7295668c059f8fce069c
sidharthkumar2019 <sidharthkumar2k19@gmail.com>
2024-05-18 11:26:22 +0530

    Updated try-catch logic of course controllers.

--- src/controller/course.js
+++ src/controller/course.js
@@ -1,91 +1,95 @@
 import {Course} from '../models/course.js';
 
 export const addCourse = async (req, res) => {
-    const {
-        name, price, description, author, enrolledCount
-    } = req.body;
-
-    let newCourse = new Course({
-        name,
-        price,
-        description,
-        author,
-        enrolledCount,
-    });
-
     try {
+        const {
+            name, price, description, author, enrolledCount
+        } = req.body;
+    
+        let newCourse = new Course({
+            name,
+            price,
+            description,
+            author,
+            enrolledCount,
+        });
         newCourse = await newCourse.save();
+        
+        return res.status(201).send(newCourse);
     } catch (error) {
         return res.status(400).json({ message: error.message });
     }
-
-    res.status(201).send(newCourse);
 };
 
 export const getCourseById = async (req, res) => {
-    const { courseID } = req.params;
-
-    if (courseID) {
-        let course;
-        try {
-            course = await Course.findOne({ _id: courseID });
-        } catch (error) {
-            return res.status(500).json({ error });
+    try {
+        const { courseID } = req.params;
+    
+        if (courseID) {
+            let course = await Course.findOne({ _id: courseID });
+            
+            if (course) {
+                return res.status(200).json({ course });
+            }
+            else {
+                return res.status(404).json({ message: 'No such course found.' });
+            }
         }
-
-        if (course)
-            res.status(200).json({ course });
-        else
-            res.status(404).json({ message: 'No such course found.' });
-    }
-    else {
-        return res.status(400).json({ error: "Params required" });
+        else {
+            return res.status(400).json({ error: "Params required" });
+        }
+    } catch (error) {
+        return res.status(500).json({ error });
     }
 }
 
 export const deleteCourse = async (req, res) => {
-    const { courseID } = req.params;
-    if (courseID) {
-        let course;
-        try {
-            course = await Course.deleteOne({ _id: courseID });
-        } catch (error) {
-            return res.status(500).json({ error });
+    try {
+        const { courseID } = req.params;
+        if (courseID) {
+            let deletedCourse = await Course.findOneAndDelete({ _id: courseID });
+            if (deletedCourse) {
+                return res.status(202).json({ course: deletedCourse });
+            }
+            else {
+                return res.status(404).json({ message: 'No such course found.' });
+            }
         }
-        if (course)
-            res.status(202).json({ course: course });
-        else
-            res.status(404).json({ message: 'No such course found.' });
+        else {
+            return res.status(400).json({ error: "Params required" });
+        }
+    } catch (error) {
+        return res.status(500).json({ error });
     }
-    else
-        return res.status(400).json({ error: "Params required" });
 }
 
 export const allCourses = async (req, res) => {
-    let courses;
     try {
-        courses = await Course.find({});
+        let courses = await Course.find({});
+        if (courses) {
+            return res.status(200).json({ courses });
+        }
+        else {
+            return res.status(400).json({ message: 'courses couldn\'t be obtained.' })
+        }
     } catch (error) {
         return res.status(400).json({ error });
     }
-    if (courses)
-        return res.status(200).json({ courses });
-    else
-        return res.status(400).json({ message: 'courses couldn\'t be obtained.' })
 }
 
 export const updateCourse = async (req, res) => {
-    const { courseID } = req.params;
     try {
+        const { courseID } = req.params;
         const updatedCourse = await Course.findByIdAndUpdate(courseID, req.body, {
             new: true, // return the updated document
             runValidators: true, // run schema validators
         });
-        if (!updatedCourse) {
+        if (updatedCourse) {
+            return res.status(200).send(updatedCourse);
+        }
+        else {
             return res.status(404).send('Course not found');
         }
-        
-        return res.status(200).send(updatedCourse);
     } catch (error) {
         return res.status(400).json({ message: error.message });
     }
